24814ba279de761c74fb7a3f720afd3995334a90
Miki Namsrai <miki.namsrai@gmail.com>
2020-10-18 14:40:20 +0200

    add stuff

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div className="sidebar bg-spotify-gray-900 w-48 flex-none overflow-y-auto flex flex-col justify-between">
-      <div>one</div>
+    <div className="sidebar bg-spotify-gray-900 w-48 flex-none overflow-y-auto flex flex-col justify-between font-semibold">
+      <ul className="py-6">
+        <li className="border-l-4 border-green-600">
+          <a href="#" className="flex items-center mx-4 mt-4">
+            <span>svg</span>
+            <span>Home</span>
+          </a>
+        </li>
+        <li className=""></li>
+        <li className=""></li>
+      </ul>
       <div className="sidebar-spotify overflow-y-auto">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae impedit dolor maiores minus hic exercitationem harum at! Totam provident accusantium numquam aperiam nostrum obcaecati, sequi at. Sed veniam, maiores, amet inventore facilis quod doloribus excepturi laborum perspiciatis quae doloremque dignissimos necessitatibus, repudiandae atque architecto? Eligendi sed hic praesentium optio dolorem expedita repellendus doloribus provident neque ipsum inventore suscipit non dolores, vitae numquam modi tempora facilis amet obcaecati eveniet earum reiciendis! Non eligendi unde exercitationem nobis excepturi dolores, consequuntur omnis iusto deleniti sapiente labore magni commodi blanditiis incidunt maiores voluptates eaque deserunt in voluptate totam, cum aperiam quis quod? Porro est asperiores enim perferendis architecto eius deserunt facilis aliquid eum optio provident beatae expedita unde, ipsam non amet iste nisi error possimus? Officiis vel, excepturi quidem accusantium itaque quam. Ipsa tenetur quisquam non vero id aut quasi obcaecati, ut possimus provident minima optio nemo numquam culpa earum labore asperiores suscipit, qui in. Eaque possimus rerum rem eveniet. Mollitia sequi totam architecto optio amet vero. Sequi eaque tenetur et nihil itaque laudantium quae, eius eveniet numquam aliquid. Magnam, pariatur quod laborum a, itaque delectus, quia laudantium fugiat repellendus eos nam quidem sapiente sint magni quas asperiores illo nulla. Animi iste officia amet et sequi aperiam saepe quae ut excepturi adipisci, distinctio ratione dolores accusantium laboriosam illo voluptatibus voluptate deserunt! Ea, officiis sed provident, maiores ipsam nostrum tenetur, natus temporibus quisquam aperiam iusto expedita dolorum dignissimos saepe non laboriosam. Quod iste dolores cum est, beatae harum explicabo, atque, itaque nihil distinctio eveniet eligendi.
       </div>
