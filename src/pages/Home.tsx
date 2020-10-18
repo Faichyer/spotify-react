d3f6bf22940e600efc5c3d44b6fafa6a08c54229
Miki Namsrai <miki.namsrai@gmail.com>
2020-10-18 13:50:16 +0200

    setup layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,9 @@ function Home() {
   return (
     <div>
       <div className="flex flex-col h-screen">
-        <div className="bg-blue-500 flex-1 flex">
-          <div className="sidebar">Sidebar</div>
-          <div className="content-area">Content</div>
+        <div className="flex-1 flex overflow-y-hidden">
+          <div className="sidebar bg-red-500 w-48 flex-none overflow-y-auto">Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia doloremque, aperiam modi maxime vero deserunt minima vitae exercitationem. Quis asperiores atque quasi illo laudantium magnam modi a quas esse vitae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla necessitatibus dolor enim perspiciatis. Possimus alias sed praesentium. Aut magni laboriosam delectus sit impedit repellat a, ipsum quasi fugit. Beatae, libero. consectetur adipisicing elit. Unde, magni temporibus, vitae ea ullam perspiciatis eius totam doloribus repellat id aperiam voluptates illum ipsam esse, fuga necessitatibus ipsum aspernatur qui. Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est tempore vitae illum sit officia eum, iusto incidunt harum sunt animi explicabo quo voluptas, quisquam aliquam ratione voluptates. Recusandae, molestiae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores saepe vitae beatae modi quia cumque rerum? Dignissimos recusandae est necessitatibus doloremque repellendus, enim nihil quis excepturi iste, pariatur, ipsum perspiciatis.</div>
+          <div className="content-area bg-red-300 flex-1 overflow-y-auto">Content Area </div>
         </div>
         <div className="bg-blue-300">bot</div>
       </div>
